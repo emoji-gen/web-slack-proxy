01a9f453174de18cdf9c4623f49d70a0f4215365
Pine Mizune <pinemz@gmail.com>
2018-06-24 13:21:40 +0900

    fix(.circleci): add commiter info

--- .circleci/setup-config.sh
+++ .circleci/setup-config.sh
@@ -2,5 +2,9 @@
 
 set -eu -o pipefail
 
+git config --global user.email 'ultimate.emoji.gen@gmail.com'
+git config --global user.name 'Emoji Generator'
+
 git rm externals/conf
 git commit -m 'fix(externals): remove private module'
+
