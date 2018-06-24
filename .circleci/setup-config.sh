90d0f1e01c89e7648c56cc8310a626776c4e6769
Pine Mizune <pinemz@gmail.com>
2018-06-24 13:29:45 +0900

    feat(.circleci): copy conf file

--- .circleci/setup-config.sh
+++ .circleci/setup-config.sh
@@ -5,6 +5,9 @@ set -eu -o pipefail
 git config --global user.email 'ultimate.emoji.gen@gmail.com'
 git config --global user.name 'Emoji Generator'
 
+# Copy app config
+cp externals/conf/app/web-slack-proxy.yml config/production.yml
+
 # Remove private submodule
 git rm externals/conf
 git commit -m 'fix(externals): remove private module'
