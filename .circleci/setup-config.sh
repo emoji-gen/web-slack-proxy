4cee6d99ce4859f654f476650eb4ba88add86997
Pine Mizune <pinemz@gmail.com>
2018-06-24 13:31:42 +0900

    fix(config): add private conf

--- .circleci/setup-config.sh
+++ .circleci/setup-config.sh
@@ -7,6 +7,8 @@ git config --global user.name 'Emoji Generator'
 
 # Copy app config
 cp externals/conf/app/web-slack-proxy.yml config/production.yml
+git add config/production.yml
+git commit -m 'fix(config): add production conf'
 
 # Remove private submodule
 git rm externals/conf
