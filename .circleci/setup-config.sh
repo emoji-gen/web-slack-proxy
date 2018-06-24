4cda493f56f555c7dd655ae915ccb403c03bef04
Pine Mizune <pinemz@gmail.com>
2018-06-24 13:23:37 +0900

    docs(setup-config.sh): add comment

--- .circleci/setup-config.sh
+++ .circleci/setup-config.sh
@@ -5,6 +5,7 @@ set -eu -o pipefail
 git config --global user.email 'ultimate.emoji.gen@gmail.com'
 git config --global user.name 'Emoji Generator'
 
+# Remove private submodule
 git rm externals/conf
 git commit -m 'fix(externals): remove private module'
 
