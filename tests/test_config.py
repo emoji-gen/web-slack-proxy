c2e88c84d39bccd7fb7f332550eee57eb7d8a209
Pine Mizune <pinemz@gmail.com>
2018-06-26 19:53:26 +0900

    test(test_config.py): use pytest-mock

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,20 +4,20 @@ from pathlib import Path
 
 import pytest
 
-from emoji_slack_proxy import config
+from emoji_slack_proxy.config import load_config
 
 
-async def test_load_config_zero(test_client):
-    config.config_path = str(Path(__file__).parents[0].joinpath('data/config0'))
-    result = config.load_config()
+async def test_load_config_zero(mocker):
+    mocker.patch('emoji_slack_proxy.config.config_path',  str(Path(__file__).parents[0].joinpath('data/config0')))
+    result = load_config()
 
     assert isinstance(result, dict)
     assert len(result) == 0
 
 
-async def test_load_config_local_product(test_client):
-    config.config_path = str(Path(__file__).parents[0].joinpath('data/config1'))
-    result = config.load_config()
+async def test_load_config_local_product(mocker):
+    mocker.patch('emoji_slack_proxy.config.config_path', str(Path(__file__).parents[0].joinpath('data/config1')))
+    result = load_config()
 
     assert isinstance(result, dict)
     assert len(result) == 3
