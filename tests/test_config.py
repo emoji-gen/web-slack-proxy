0722b603fc7d8fff3712379f49956ad1d468b099
Pine Mizune <pinemz@gmail.com>
2018-06-26 19:39:32 +0900

    test(config): add config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,10 +7,19 @@ import pytest
 from emoji_slack_proxy import config
 
 
-async def test_load_config(test_client):
-    config.config_path = str(Path(__file__).parents[0].joinpath('data'))
+async def test_load_config_zero(test_client):
+    config.config_path = str(Path(__file__).parents[0].joinpath('data/config0'))
     result = config.load_config()
 
+    assert isinstance(result, dict)
+    assert len(result) == 0
+
+
+async def test_load_config_local_product(test_client):
+    config.config_path = str(Path(__file__).parents[0].joinpath('data/config1'))
+    result = config.load_config()
+
+    assert isinstance(result, dict)
     assert len(result) == 3
     assert result['foo'] == 'foo'
     assert result['bar'] == True
